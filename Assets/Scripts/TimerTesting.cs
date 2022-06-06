0be906e217e8ee6a6d85496c48431bb8eac31ba1
KostyaTyan <koalla911@gmail.com>
2022-06-06 19:17:38 +0300

    testing timer

---
+++ Assets/Scripts/TimerTesting.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace CosmoSoup
+{
+	public class TimerTesting : MonoBehaviour
+	{
+		public float timer = 3f;
+		private float timeCount;
+		private bool isPaused = false;
+
+		private void Update()
+		{
+			if (isPaused == false)
+			{
+				timeCount -= Time.deltaTime;
+				if (timeCount < 0)
+				{
+					Debug.Log("timer");
+					timeCount = timer;
+				}
+			}
+		}
+
+		public void TimerPaused()
+		{
+			isPaused = !isPaused;
+		}
+	}
+}
